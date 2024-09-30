73d8a560eee9277e03d0ef7f7d11b260f2d56606
GrabinaSergey <dieumortx@gmail.com>
2024-09-30 19:00:50 +0500

    Task1

---
+++ Tyuiu.GrabinaSA.Sprint1.Task1.V30/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.GrabinaSA.Sprint1.Task1.V30.Lib;
+namespace Tyuiu.GrabinaSA.Sprint1.Task1.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Грабина С. А. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил Грабина Сергей Александрович | ИСТНб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (x * 2)/(1 + x) и печатает его на экране *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(ds.Calculate(x));
+            Console.ReadLine();
+        }
+    }
+}
