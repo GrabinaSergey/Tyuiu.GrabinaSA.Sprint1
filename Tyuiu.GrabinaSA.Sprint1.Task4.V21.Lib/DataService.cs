db588910e56c8f88ddc2b6fc66c5824bc8db9206
GrabinaSergey <dieumortx@gmail.com>
2024-09-30 21:57:00 +0500

    Task4

---
+++ Tyuiu.GrabinaSA.Sprint1.Task4.V21.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.GrabinaSA.Sprint1.Task4.V21.Lib
+{
+    public class DataService : ISprint1Task4V21
+    {
+        public double Calculate(double x, double y)
+        {
+            return Math.Round(Math.Abs(Math.Pow(1 + x, 2) - y)/(x + y), 3);
+        }
+    }
+}
